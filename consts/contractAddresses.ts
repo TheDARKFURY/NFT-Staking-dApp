a2bbe226640f796df89140dc8100ca23fa70ed52
Soham Shah <sohamshah419@gmail.com>
2023-03-16 02:20:51 +0530

    Contract deployed and address added

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
-  "0x86eAf8D635eb11D5CD801c41744F44e50d4fd8d2";
+  "0x8F618c9Eb7805FcC81D734934abD529dd464955C";
 export const tokenContractAddress =
-  "0x12ea05ba6E8405cBeceB5b51134A821e86858F7E";
+  "0xbd31bd1D2c25BAb5ad9134C6E3840DF9cE9DbE23";
 export const stakingContractAddress =
-  "0x830Ace54DFbf3a011601bD0Bc1f9806d45D2d0E4";
+  "0xbA2A2c2EF9a797fD618F0BE9bD7ba6882F727C93";
